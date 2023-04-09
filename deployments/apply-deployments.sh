f205d42827ff5de1ec7c74f9e22392a7f7b558d6
Oskar Baginski <oskbagin@gmail.com>
2023-04-09 11:55:51 +0200

    Script to apply all the deployments and some fixes

---
+++ deployments/apply-deployments.sh
@@ -0,0 +1,13 @@
+# Kafka
+kubectl apply -f kafka/kafka-ns.yaml
+kubectl apply -f kafka/zookeeper.yaml
+kubectl apply -f kafka/kafka.yaml
+
+# CouchDB
+kubectl apply -f couchdb/couchdb-namespace.yaml
+kubectl apply -f couchdb/couchdb-secret.yaml
+kubectl apply -f couchdb/couchdb.yaml
+
+# NodeRED
+kubectl apply -f node-red/node-red-namespace.yaml
+kubectl apply -f node-red/node-red.yaml
