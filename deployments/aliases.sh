4112d58c961c0038c20edd84f0829c69aab25809
oskaerd <oskbagin@gmail.com>
2023-04-10 19:32:14 +0200

    Fixes

--- deployments/aliases.sh
+++ deployments/aliases.sh
@@ -2,3 +2,6 @@ alias k=kubectl
 alias cls=clear
 alias python=python3
 alias py=python3
+check_port() {
+    sudo ss -lptn "sport = :$1"
+}
--- deployments/apply-deployments.sh
+++ deployments/apply-deployments.sh
@@ -2,12 +2,10 @@
 kubectl apply -f kafka/kafka-namespace.yaml
 kubectl apply -f kafka/zookeeper.yaml
 kubectl apply -f kafka/kafka.yaml
-
 # CouchDB
 kubectl apply -f couchdb/couchdb-namespace.yaml
 kubectl apply -f couchdb/couchdb-secret.yaml
 kubectl apply -f couchdb/couchdb.yaml
-
 # NodeRED
 kubectl apply -f node-red/node-red-namespace.yaml
 kubectl apply -f node-red/node-red.yaml
