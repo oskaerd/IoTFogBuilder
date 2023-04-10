ea8802e6faa690033c15c5c80da06f60f4950988
oskaerd <oskbagin@gmail.com>
2023-04-10 22:13:43 +0200

    Parametrized input for the kafka scripts

--- deployments/scripts/kafka-consumer.py
+++ deployments/scripts/kafka-consumer.py
@@ -1,13 +1,14 @@
-import time
 from kafka import KafkaConsumer
+import time
+import sys
 
 if __name__ == "__main__":
     print("Running consumer")
     records = []
-    topic_name = "books"
+    topic_name = sys.argv[3]
 
     consumer = KafkaConsumer(topic_name, auto_offset_reset='earliest',
-                             bootstrap_servers=['192.168.0.100:19092'], api_version=(0, 10), consumer_timeout_ms=1000)
+                             bootstrap_servers=[f'{sys.argv[1]}:{sys.argv[2]}'], api_version=(0, 10), consumer_timeout_ms=1000)
     for msg in consumer:
         message_text = msg.value
         records.append(message_text)
--- deployments/scripts/kafka-producer.py
+++ deployments/scripts/kafka-producer.py
@@ -1,14 +1,11 @@
 from kafka import KafkaProducer
-
+import sys
 
 def publish_message(producer_instance, topic_name, key, value):
     try:
-        print("b")
         key_bytes = bytes(key, encoding='utf-8')
         value_bytes = bytes(value, encoding='utf-8')
-        print("c")
         producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
-        print("d")
         producer_instance.flush()
         print('Message published successfully.')
     except Exception as ex:
@@ -19,7 +16,7 @@ def publish_message(producer_instance, topic_name, key, value):
 def connect_kafka_producer():
     _producer = None
     try:
-        _producer = KafkaProducer(bootstrap_servers=['192.168.0.100:19092'], api_version=(0, 10))
+        _producer = KafkaProducer(bootstrap_servers=[f'{sys.argv[1]}:{sys.argv[2]}'], api_version=(0, 10))
     except Exception as ex:
         print('Exception while connecting Kafka')
         print(str(ex))
@@ -27,10 +24,10 @@ def connect_kafka_producer():
         return _producer
 
 if __name__ == '__main__':
-    all_recipes = ["aaaaa", "bbbbbb", "ccccccc", "dexi", "eeeeeeee"]
-    kafka_producer = connect_kafka_producer()
-    for recipe in all_recipes:
-        print("A")
-        publish_message(kafka_producer, 'books', 'raw', recipe.strip())
-    if kafka_producer is not None:
-        kafka_producer.close()
+    with open(f'{sys.argv[4]}', 'r') as input_file:
+        records_to_send = input_file.readlines()
+        kafka_producer = connect_kafka_producer()
+        for recipe in records_to_send:
+            publish_message(kafka_producer, sys.argv[3], 'raw', recipe.strip())
+        if kafka_producer is not None:
+            kafka_producer.close()
