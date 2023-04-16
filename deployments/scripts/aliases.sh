8cf74d231a768186b1670d98df199e4960757e70
Oskar Baginski <oskbagin@gmail.com>
2023-04-16 18:50:56 +0200

    Alias to forward port because NodePort doesn't work for Kafka for some reason

--- deployments/scripts/aliases.sh
+++ deployments/scripts/aliases.sh
@@ -8,3 +8,9 @@ alias py=python3
 check_port() {
     sudo ss -lptn "sport = :$1"
 }
+# Arguments: pod_name, port number, IP address(es)
+port_forward() {
+    while true;
+    do kubectl port-forward $1 $2 --address $3;
+    done > /dev/null 2>&1 &
+}
