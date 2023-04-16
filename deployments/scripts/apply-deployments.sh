849f4f9c6a986dc7a09dea746c96915c3554f988
Oskar Baginski <oskbagin@gmail.com>
2023-04-16 18:55:12 +0200

    Added new deployments to the apply-deployments.sh script

--- deployments/scripts/apply-deployments.sh
+++ deployments/scripts/apply-deployments.sh
@@ -2,13 +2,17 @@
 # Example usage:
 #   ./apply-deployments.sh
 # Kafka
-kubectl apply -f kafka/kafka-namespace.yaml
-kubectl apply -f kafka/zookeeper.yaml
-kubectl apply -f kafka/kafka.yaml
+kubectl apply -f ../kafka/kafka-namespace.yaml
+kubectl apply -f ../kafka/zookeeper.yaml
+kubectl apply -f ../kafka/kafka.yaml
+kubectl apply -f ../kafka/kafdrop.yaml
 # CouchDB
-kubectl apply -f couchdb/couchdb-namespace.yaml
-kubectl apply -f couchdb/couchdb-secret.yaml
-kubectl apply -f couchdb/couchdb.yaml
+kubectl apply -f ../couchdb/couchdb-namespace.yaml
+kubectl apply -f ../couchdb/couchdb-secret.yaml
+kubectl apply -f ../couchdb/couchdb.yaml
 # NodeRED
-kubectl apply -f node-red/node-red-namespace.yaml
-kubectl apply -f node-red/node-red.yaml
+kubectl apply -f ../node-red/node-red-namespace.yaml
+kubectl apply -f ../node-red/node-red.yaml
+# Grafana
+kubectl apply -f ../grafana/grafana-namespace.yaml
+kubectl apply -f ../grafana/grafana.yaml
