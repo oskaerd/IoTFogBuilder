cc11942cb2f85cefbcf83b9c7678b4b7d454ef26
Oskar Baginski <oskbagin@gmail.com>
2023-06-22 21:01:57 +0200

    deploy

--- deployments/scripts/apply-deployments.sh
+++ deployments/scripts/apply-deployments.sh
@@ -2,10 +2,10 @@
 # Example usage:
 #   ./apply-deployments.sh
 # Kafka
-kubectl apply -f ../kafka/kafka-namespace.yaml
-kubectl apply -f ../kafka/zookeeper.yaml
-kubectl apply -f ../kafka/kafka.yaml
-kubectl apply -f ../kafka/kafdrop.yaml
+# kubectl apply -f ../kafka/kafka-namespace.yaml
+# kubectl apply -f ../kafka/zookeeper.yaml
+# kubectl apply -f ../kafka/kafka.yaml
+# kubectl apply -f ../kafka/kafdrop.yaml
 # CouchDB
 kubectl apply -f ../couchdb/couchdb-namespace.yaml
 kubectl apply -f ../couchdb/couchdb-secret.yaml
@@ -14,4 +14,8 @@ kubectl apply -f ../couchdb/couchdb.yaml
 kubectl apply -f ../node-red/node-red-namespace.yaml
 kubectl apply -f ../node-red/node-red.yaml
 # Prometheus/Grafana
+chmod +x ../grafana/deploy-prometheus-grafana.sh
 ../grafana/deploy-prometheus-grafana.sh
+
+# rabbit
+helm install my-release oci://registry-1.docker.io/bitnamicharts/rabbitmq --set auth.username="guest" --set auth.password="guest"
