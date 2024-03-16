397603bf08f52bb302ffc27290ccbd71fd9ea724
Oskar Baginski <oskbagin@gmail.com>
2024-03-16 15:01:32 +0100

    Removed unused grafana files

--- deployments/grafana/deploy-prometheus-grafana.sh
+++
@@ -1,8 +0,0 @@
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm install prometheus --namespace prometheus --create-namespace prometheus-community/kube-prometheus-stack 
-echo
-echo
-echo "########### Now run: ###########"
-echo "kubectl edit service prometheus-grafana -n prometheus"
-echo "And change service type from ClusterIp to NodePort"
-echo "And set nodePort to desired exposed port (our setting is 30000)"
